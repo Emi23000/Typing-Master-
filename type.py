a6806a484b1c5adbdfaec4366cce76b5eaf4d537
emirethsoto <emishddez30@gmail.com>
2026-02-19 19:31:53 -0400

    agregar listras y poner el resultado final

--- type.py
+++ type.py
@@ -1,2 +1,19 @@
 print ("Random Word Game")
 print ("Lets start with the 1st round")
+
+list = [
+    "Colombia"
+    "Mexico"
+    "Guatemala"
+    "España"
+    "Alemania"
+    "Argentina"
+    "Chile"
+    "Brasil"
+    "Bolivia"
+    "Honduras"
+    "Panama"
+    "Italia"
+    "Francia"
+]
+
