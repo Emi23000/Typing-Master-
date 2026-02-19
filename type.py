6dba70095e9c180db4a3746ab746319cc294f469
emirethsoto <emishddez30@gmail.com>
2026-02-19 19:53:56 -0400

    poner el ejemplo del profe y poner la parte de que hacer cuando sea correct el random word y cuando no el que poer, tambien agregue la ultima parte

--- type.py
+++ type.py
@@ -2,18 +2,33 @@ print ("Random Word Game")
 print ("Lets start with the 1st round")
 
 list = [
-    "Colombia"
-    "Mexico"
-    "Guatemala"
-    "España"
-    "Alemania"
-    "Argentina"
-    "Chile"
-    "Brasil"
-    "Bolivia"
-    "Honduras"
-    "Panama"
-    "Italia"
-    "Francia"
+    "Colombia",
+    "Mexico",
+    "Guatemala",
+    "España",
+    "Alemania",
+    "Argentina",
+    "Chile",
+    "Brasil",
+    "Bolivia",
+    "Honduras",
+    "Panama",
+    "Italia",
+    "Francia",
 ]
 
+randomOrder = random.shuffle(list) 
+
+
+if user_input == word:
+    print("Correct!")
+    correct_word += 1
+else:
+    print("Incorrect! Try again. Choose wisely.")
+    incorrect_word += 1
+accuracy = (correct_word / len(words)) * 100
+
+print("Game Summary")
+print("Total correct words:", correct_word)
+print("Total incorrect words:", incorrect_word)
+print("Results:", accuracy, "%")
